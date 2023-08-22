c6e0900b82138e267029ebb2b1a8f7da3605f8b7
vincent <uveghobamien@gmail.com>
2023-08-22 18:07:13 +0100

    responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import Head from 'next/head'
+Head
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <Head>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+       <body className={`${inter.className}`}>{children}</body>
+      </Head>
+      
     </html>
   )
 }
